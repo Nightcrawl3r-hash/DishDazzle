38babd24498c42302d9619706e7757f45ae38175
Naresh Rajbanshi <nar3sh0@gmail.com>
2023-11-10 18:58:33 +0545

    responsive done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
@@ -6,14 +6,18 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
   const [error, setError] = useState("");
-  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+  // const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+
 
   useEffect(() => {
     async function fetchData() {
       try {
         setLoading(true);
         setError("");
-        const res = await fetch(URL);
+        // maile yo url change gareko matra ho
+          const res = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+        );
         if (!res.ok) {
           throw new Error("Something went wrong with fetching data...");
         }
@@ -30,17 +34,17 @@ function App() {
         setLoading(false);
       }
     }
-    if (query.trim().length < 2) {
+    if (query.trim().length < 3) {
       setResults([]);
       setError("");
       return;
     }
     fetchData();
-  }, [URL]);
+  }, [query]);
 
   return (
     <>
-      <div className=" h-screen  ">
+      <div className="  ">
         <nav className="fixed bg-gray-400 w-full z-20">
           <div className="container m-auto p-2 flex items-center justify-between gap-x-4">
             <a href="#" className="flex-col px-2 items-center justify-center ">
@@ -74,7 +78,7 @@ function App() {
             {!query ? (
               <>
                 <div className="h-screen flex items-center justify-center">
-                  <p className=" text-5xl text-gray-300 text-center mx-20">
+                  <p className=" text-5xl text-gray-300 text-center mx-4 md:mx-20">
                     Hungry? 🍽️ Type in a dish or ingredient you're craving and
                     let our search find the perfect recipe for you!
                   </p>
@@ -87,11 +91,11 @@ function App() {
                 <div className="h-max ">
                   {results.map((result) => (
                     <div
-                      className="grid grid-cols-2 gap-6 items-center justify-center my-28 p-8"
+                      className="grid lg:grid-cols-2 gap-6 items-center justify-center py-16 lg:my-24 p-8"
                       key={result.idMeal}
                     >
-                      <div className="">
-                        <h2 className="text-7xl text-shadow-lg text-white font-bold">
+                      <div className="lg:order-first">
+                        <h2 className=" text-4xl md:text-7xl text-shadow-lg text-white font-bold">
                           {result.strMeal}
                         </h2>
                         <h2 className="text-xl text-gray-200 py-1">
@@ -112,17 +116,18 @@ function App() {
                           {result.strInstructions}
                         </p>
                       </div>
-                      <div className="p-2 border-dotted border-4 border-gray-50  rounded-full">
+                      <div className="order-first flex justify-center p-12 md:p-20">
                         <img
                           src={result.strMealThumb}
                           alt="meal-image"
-                          className=" animate-spin-slow rounded-full shadow-lg contrast-100 transform transition-transform hover:scale-110 "
+                          className=" animate-spin-slow rounded-full shadow-2xl contrast-100 transform transition-transform hover:scale-110 h-max "
                         />
                       </div>
                       <hr />
                     </div>
                   ))}
                 </div>
+                <Footer/>
               </>
             ) : (
               <NotFound />
@@ -138,7 +143,7 @@ function Loader() {
   return (
     <div className="">
       <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-blue-500"></div>
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
       </div>
     </div>
   );
@@ -157,15 +162,25 @@ function NotFound() {
 }
 
 function NumResults({ results }) {
-  const [num, setNum] = useState(0);
+  const [nums, setNum] = useState(0);
   useEffect(() => {
     setNum(results ? results.length : 0);
   }, [results]);
   return (
-    <p className="text-xl text-gray-900">
-      Found <strong>{num}</strong> results
+    <p className="md:text-xl text-gray-900">
+      Found <strong>{nums}</strong> results
     </p>
   );
+}
+
+function Footer(){
+  return(
+    <>
+    <div className="container m-auto">
+      <p className=" text-gray-300 text-center">&copy; Naresh Rajbanshi 2023. All rights reserved. </p>
+    </div>
+    </>
+  )
 }
 
 export default App;
